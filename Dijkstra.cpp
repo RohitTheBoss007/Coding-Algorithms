24e193df48ca413ca3a271a1378323f61e9d14d5
Rohit Pal <50323148+RohitTheBoss007@users.noreply.github.com>
2020-02-27 02:13:49 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,103 +1,45 @@
-// Program to find Dijkstra's shortest path using 
-// priority_queue in STL 
-#include<bits/stdc++.h> 
-using namespace std; 
-# define INF 0x3f3f3f3f 
-
-// iPair ==> Integer Pair 
-typedef pair<int, int> iPair; 
-int n,m,k;
-
-
-class Graph 
-{ 
-    int V; // No. of vertices 
-
-    // In a weighted graph, we need to store vertex 
-    // and weight pair for every edge 
-    list< pair<int, int> > *adj; 
-
-public: 
-    Graph(int V); // Constructor 
-
-    // function to add an edge to graph 
-    void addEdge(int u, int v, int w); 
-
-    // prints shortest path from s 
-    void shortestPath(int s); 
-}; 
-
-// Allocates memory for adjacency list 
-Graph::Graph(int V) 
-{ 
-    this->V = V; 
-    adj = new list<iPair> [V]; 
-} 
-
-void Graph::addEdge(int u, int v, int w) 
-{ 
-    adj[u].push_back(make_pair(v, w)); 
-    adj[v].push_back(make_pair(u, w)); 
-} 
-
-// Prints shortest paths from src to all other vertices 
-void Graph::shortestPath(int src) 
-{ 
-    
-    priority_queue< iPair, vector <iPair> , greater<iPair> > pq; 
-
-   
-    vector<int> dist(V, INF); 
-
-    pq.push(make_pair(0, src)); 
-    dist[src] = 0; 
-
-    while (!pq.empty()) 
-    { 
-       
-        int u = pq.top().second; 
-        pq.pop(); 
-
-        
-        list< pair<int, int> >::iterator i; 
-        for (i = adj[u].begin(); i != adj[u].end(); ++i) 
-        { 
-            // Get vertex label and weight of current adjacent 
-            // of u. 
-            int v = (*i).first; 
-            int weight = (*i).second; 
-
-            // If there is shorted path to v through u. 
-            if (dist[v] > dist[u] + weight) 
-            { 
-                // Updating distance of v 
-                dist[v] = dist[u] + weight; 
-                pq.push(make_pair(dist[v], v)); 
-            } 
-        } 
-    } 
-
-    
-    printf("Vertex Distance from Source\n"); 
-    for (int i = 0; i < V; ++i) 
-        printf("%d \t\t %d\n", i, dist[i]); 
-} 
-
-
-int main() 
-{ 
-    // k is dummy input (ignore)
-    cin>>n>>m>>k;
-    Graph g(n);
-    for(int i=0;i<m;i++)
-    {
-        int x,y,w;
-        cin>>x>>y>>w;
-        x--;y--;
-        g.addEdge(x,y,w);
-
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector< pii > vii;
+#define INF 0x3f3f3f3f
+vii *G;    // Graph
+vi D;      // distance vector for storing min distance from the source.
+/*=======================================*/
+void dijkstra(int source, int N) {
+    priority_queue<pii, vector<pii>, greater<pii> > Q;   // min heap
+    D.assign(N,INF);
+    D[source] = 0;
+    Q.push({0,source});
+    while(!Q.empty()){
+        int u = Q.top().second;
+        Q.pop();
+        for(auto &c : G[u]){
+            int v = c.second;
+            int w = c.first;
+            if(D[v] > D[u]+w){
+                D[v] = D[u]+w;
+                Q.push({D[v],v});
+            }
+        }
     }
-    g.shortestPath(0); 
-
-    return 0; 
-} 
+}
+/*==========================================*/
+int main(){
+    int N,M,k,u,v,w;
+    cin >> N >> M >> k; 
+//Input the number of nodes(0 based), number of edges and the source vertex.
+    G = new vii[N];
+    for(int i=0;i<M;i++){
+        cin >> u >> v >> w; 
+        u--;v--;
+//Input the starting vertex of the edge, the ending vertex and the cost of the edge.
+        G[u].push_back({w,v});
+        G[v].push_back({w,u});
+    }
+    dijkstra(0, N); 
+ 
+    cout<<D[N-1];
+    cout<<endl;
+}
