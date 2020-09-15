6b9506b61b2d7571a5275eadb71fa3e857af709c
Rohit Pal <50323148+RohitTheBoss007@users.noreply.github.com>
2020-09-15 14:27:59 +0530

    Update modintlib.cpp

--- modintlib.cpp
+++ modintlib.cpp
@@ -1,3 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define ll long long
+#define fast  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define f(i,n) for(ll i=0;i<n;i++)
+#define fore(i, a, b) for (ll i = (ll)(a); i <= (ll)(b); ++i)
+#define nl "\n"
+#define trace(x) cerr<<#x<<": "<<x<<" "<<endl
+#define trace2(x, y) cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z) cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define x first
+#define y second
+#define vc vector
+#define pb push_back
+#define ar array
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ms(v,n,x) fill(v,v+n,x);
+#define init(c,a) memset(c,a,sizeof(c))
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define sll set<ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define inf 9e18
+#define cases ll T;cin>>T;while(T--)
+#define BLOCK 500
+//const double PI = 3.141592653589793238460;
+template<typename T> bool mmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template<typename T> bool mmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+// void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
 template <typename T>
 T inverse(T a, T m) {
   T u = 0, v = 1;
@@ -162,5 +226,20 @@ ModType& md = VarMod::value;
 using Mint = Modular<VarMod>;
 */
  
-constexpr int md = (int) 1e9 + 7;
+constexpr int md = 998244353;
 using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+ 
+vector<Mint> fact(1, 1);
+vector<Mint> inv_fact(1, 1);
+ 
+Mint C(int n, int k) {
+  if (k < 0 || k > n) {
+    return 0;
+  }
+  while ((int) fact.size() < n + 1) {
+    fact.push_back(fact.back() * (int) fact.size());
+    inv_fact.push_back(1 / fact.back());
+  }
+  return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
+ 
