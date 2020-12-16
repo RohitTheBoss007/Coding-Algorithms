486f13e13bfe98a85948faf493bf9134063275d2
Rohit Pal <50323148+RohitTheBoss007@users.noreply.github.com>
2020-12-17 00:17:18 +0530

    Create PersistentDSU.cpp

---
+++ PersistentDSU.cpp
@@ -0,0 +1,206 @@
+// Problem: Persistent UnionFind
+// Contest: Library Checker
+// Time: 2020-12-16 23:45:07
+
+// 私に忍び寄るのをやめてください、ありがとう
+
+#include<bits/stdc++.h>
+using namespace std;
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define ll long long
+#define int long long
+#define mod 1000000007 //998244353
+#define fast  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define f(i,n) for(ll i=0;i<n;i++)
+#define fore(i, a, b) for (ll i = (ll)(a); i <= (ll)(b); ++i)
+#define nl "\n"
+#define trace(x) cerr<<#x<<": "<<x<<" "<<endl
+#define trace2(x, y) cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z) cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define x first
+#define y second
+#define vc vector
+#define pb push_back
+#define ar array
+#define all(a) (a).begin(), (a).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ms(v,n,x) fill(v,v+n,x);
+#define init(c,a) memset(c,a,sizeof(c))
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define sll set<ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define inf 9e18
+#define cases ll T;cin>>T;while(T--)
+#define BLOCK 500
+//const double PI = 3.141592653589793238460;
+template<typename T> bool mmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template<typename T> bool mmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+// void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+template< typename T, int LOG >
+struct PersistentArray {
+  struct Node {
+    T data;
+    Node *child[1 << LOG] = {};
+
+    Node() {}
+
+    Node(const T &data) : data(data) {}
+  };
+
+  Node *root;
+
+  PersistentArray() : root(nullptr) {}
+
+  T get(Node *t, int k) {
+    if(k == 0) return t->data;
+    return get(t->child[k & ((1 << LOG) - 1)], k >> LOG);
+  }
+
+  T get(const int &k) {
+    return get(root, k);
+  }
+
+  pair< Node *, T * > mutable_get(Node *t, int k) {
+    t = t ? new Node(*t) : new Node();
+    if(k == 0) return {t, &t->data};
+    auto p = mutable_get(t->child[k & ((1 << LOG) - 1)], k >> LOG);
+    t->child[k & ((1 << LOG) - 1)] = p.first;
+    return {t, p.second};
+  }
+
+  T *mutable_get(const int &k) {
+    auto ret = mutable_get(root, k);
+    root = ret.first;
+    return ret.second;
+  }
+
+  Node *build(Node *t, const T &data, int k) {
+    if(!t) t = new Node();
+    if(k == 0) {
+      t->data = data;
+      return t;
+    }
+    auto p = build(t->child[k & ((1 << LOG) - 1)], data, k >> LOG);
+    t->child[k & ((1 << LOG) - 1)] = p;
+    return t;
+  }
+
+  void build(const vector< T > &v) {
+    root = nullptr;
+    for(int i = 0; i < v.size(); i++) {
+      root = build(root, v[i], i);
+    }
+  }
+};
+
+/*
+ * @brief Persistent-Union-Find(永続Union-Find)
+ */
+struct PersistentUnionFind {
+  PersistentArray< int, 3 > data;
+
+  PersistentUnionFind() {}
+
+  PersistentUnionFind(int sz) {
+    data.build(vector< int >(sz, -1));
+  }
+
+  int find(int k) {
+    int p = data.get(k);
+    return p >= 0 ? find(p) : k;
+  }
+
+  int size(int k) {
+    return (-data.get(find(k)));
+  }
+
+  bool unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x == y) return false;
+    auto u = data.get(x);
+    auto v = data.get(y);
+
+    if(u < v) {
+      auto a = data.mutable_get(x);
+      *a += v;
+      auto b = data.mutable_get(y);
+      *b = x;
+    } else {
+      auto a = data.mutable_get(y);
+      *a += u;
+      auto b = data.mutable_get(x);
+      *b = y;
+    }
+    return true;
+  }
+};
+
+int32_t main()
+{
+    
+	fast	
+	cout << fixed << setprecision(12);
+	
+	ll n,q;
+	cin>>n>>q;
+	
+	vc<PersistentUnionFind> uf(q+1);
+	
+	uf[0]= PersistentUnionFind(n);
+	
+	fore(i,1,q){
+		ll t,k,u,v;
+		cin>>t>>k>>u>>v;k++;
+		if(t==0){
+			uf[i]=uf[k];
+			uf[i].unite(u,v);
+		}
+		else{
+			if(uf[k].find(u)==uf[k].find(v))cout<<1<<nl;
+			else cout<<0<<nl;
+		}	
+		
+	}
+
+    
+
+	return 0;
+    
+}
